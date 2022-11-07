68ebc151da1489c09717c3aeed177c52e76ed0ac
Jhems Wallace <jhekwall@ut.utm.edu>
2022-11-07 13:44:08 -0600

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file main.cpp
+ * @author Kenneth Wallace
+ * @date 2022-11-07
+ * @brief 
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+
+bool monty_hall();
+
+void simulateStay(int it);
+
+void simulateSwitch(int it);
+
+int main() {
+  
+  
+  return 0;
+}
